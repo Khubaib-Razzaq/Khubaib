7cff4be1b580160532a8a5747ac69b905000382a
yahyakamran <yahyakamran099@gmail.com>
2024-03-16 22:18:53 +0500

    Remove Intro

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import Contact from "./components/Contact.jsx"
 import Work from "./components/work.jsx"
 import About from "./components/About.jsx"
 import Skills from "./components/Skills.jsx"
-import Intro from "./components/intro.jsx"
 import Certificate from "./components/Certificate.jsx"
 function App() {
   return (
       <>
         <About/>
-        <Intro/>
         <Work/>
         <Certificate/>
         <Skills/>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,37 +4,19 @@ import { icons } from "../Constants/constants";
 const SocialLinks = () => {
   return (
     <div className="w-full h-[100px] bg-grayscale-950 flex justify-center items-center gap-3 p-6">
-      <a href="https://github.com/yahyakamran" target="_blank">
+      <a href="https://www.upwork.com/freelancers/khubaibr" target="_blank">
         <FontAwesomeIcon
           className="text-3xl text-grayscale-50 transition-all duration-75"
           icon={icons.faUpwork}
         />
       </a>
-<a href="https://github.com/yahyakamran" target="_blank">
-        <FontAwesomeIcon
-          className="text-3xl text-grayscale-50 transition-all duration-75"
-          icon={icons.faInstagram}
-        />
-      </a>
-<a href="https://github.com/yahyakamran" target="_blank">
-        <FontAwesomeIcon
-          className="text-3xl text-grayscale-50 transition-all duration-75"
-          icon={icons.faDiscord}
-        />
-      </a>
-<a href="https://github.com/yahyakamran" target="_blank">
-        <FontAwesomeIcon
-          className="text-3xl text-grayscale-50 transition-all duration-75"
-          icon={icons.faFacebook}
-        />
-      </a>
-      <a
-        href="https://www.linkedin.com/in/yahya-kamran-36bb9b270/"
+     <a
+        href="https://www.facebook.com/profile.php?id=100078945803270"
         target="_blank"
       >
         <FontAwesomeIcon
           className="text-3xl text-grayscale-50 transition-all duration-75"
-          icon={icons.faLinkedin}
+          icon={icons.faFacebook}
         />
       </a>
     </div>
